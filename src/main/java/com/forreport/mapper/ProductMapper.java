c0b3a6ab9c2de7c486200b19a663ff62497ea4e6
82104 <82104@MSI>
2021-01-19 02:04:21 +0900

    [update] - 제품 상세보기, 제품 리스트 구현 완료(썸네일 클릭 후 확대 작업은 제품 업로드 구현 후 진행 예정) - 제품 상세 보기 내용 구현     - view.jsp         - 완료: 별점 입력, 별점 출력, 댓글 입력, 댓글 출력, 댓글 페이징, 제품 상세보기         - 미완료: 썸네일 클릭하면 확대해서 보기(제품 업로드 구현 후 처리 예정)     - style.css 수정(barfiller 길이 조정)     - ListService, ListServiceImpl 변경 → ProductService, ProductServiceImpl     - ReviewPageCriteria, ReviewMapper, ReviewPageDTO, ReviewService, ReviewServiceImpl, ReviewVO - 상품 리스트 수정     - list.jsp, ProductController, ListService, ListServiceImpl, : 제품 옆 별점 보기 및 댓글 개수 보기 구현 - 기타 수정: header.jsp, ProductMapper

--- src/main/java/com/forreport/mapper/ProductMapper.java
+++ src/main/java/com/forreport/mapper/ProductMapper.java
@@ -7,7 +7,13 @@ import com.forreport.domain.SearchingVO;
 
 public interface ProductMapper {
 	
+	/*전체 리스트 가져오기*/
 	public List<ProductVO> getProductListWithPaging(SearchingVO searchingVO);
+	
+	/*전체 게시글 개수 가져오기*/
 	public int getTotalCount(SearchingVO searchingVO);
+	
+	/*상품 상세페이지 만들기 위해서 tbl_product 한 행의 정보 가져오기*/
+	public ProductVO getProduct(int pronum);
 	
 }
--- src/main/java/com/forreport/service/ProductService.java
+++ src/main/java/com/forreport/service/ProductService.java
@@ -5,11 +5,14 @@ import java.util.List;
 import com.forreport.domain.ProductVO;
 import com.forreport.domain.SearchingVO;
 
-public interface ListService {
+public interface ProductService {
 	
 	/*검색조건을 넣어 ProductList를 가져온다.*/
 	public List<ProductVO> getProductListWithPaging(SearchingVO searchingVO);
 	
 	/* 조건에 맞는 전체 게시글의 개수를 가져온다.*/
 	public int getTotal(SearchingVO searchingVO);
+	
+	/* pronum과 일치하는 제품 상세 정보 가져오기*/
+	public ProductVO getProduct(int pronum);
 }

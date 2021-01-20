9f4c7e6d14f7799c9304b7003f5ea65ec10cc2cd
joeun <joeun@DESKTOP-AU30NC7>
2021-01-21 00:02:22 +0900

    [add] 이메일 인증, 회원가입 폼

--- src/main/java/com/forreport/controller/UserController.java
+++ src/main/java/com/forreport/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.forreport.controller;
 
+
+import java.util.Random;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.forreport.domain.UserVO;
 import com.forreport.service.UserService;
@@ -17,38 +26,110 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class UserController {
 
-	UserService userService;
+	@Autowired
+	private UserService userService;
 
-	// 이용 약관 get
-	@GetMapping("/provision.fr")
-	public void getProvision() throws Exception {
-		log.info("get provision");
+	@Autowired
+	private JavaMailSender mailSender;
+
+
+	// 이용 약관 페이지 이동
+	@RequestMapping("/provision.fr")
+	public void provision() throws Exception {
+		log.info("provision");
+	}
+	
+	
+	// 이메일 인증 페이지 이동
+	@RequestMapping("/certification.fr")
+	public void certification() throws Exception {
+		log.info("certification");
 	}
 
-	// 이용 약관 post
-	@PostMapping("/provision.fr")
-	public String postProvision(UserVO vo) throws Exception {
-		log.info("post provision");
+	// 이메일 중복 체크
+	@PostMapping("/emailCheck.fr")
+	@ResponseBody
+	public String emailCheck(String email) throws Exception {
 
-		userService.join(vo);
+		int result = userService.emailCheck(email);
 
-		return "redirect:/user/join.fr";
+		if (result != 0) {
+			return "fail"; // 중복아이디 존재
+		} else {
+			return "success";
+		}
 	}
 
-	// 회원 가입 get
-	@GetMapping("/join.fr")
-	public void getJoin() throws Exception {
-		log.info("get join");
+
+	// 본인 인증 (메일 발송) 
+	@GetMapping("/certificationProcess.fr")
+	@ResponseBody
+	public String certificationProcess(String email) throws Exception {
+		
+		log.info("이메일 = "+email);
+
+		// 이메일로 받는 인증코드 부분 (난수)
+		Random r = new Random();
+		int checkNum = r.nextInt(888888) + 111111;
+
+		log.info("인증번호 = " + checkNum);
+
+		// 이메일 발송
+		String setFrom = "forreport0202@gmail.com";
+		String toMail = email;
+		String title = "[For Report] 회원가입 인증 이메일 입니다.";
+		String content = "홈페이지를 방문해주셔서 감사합니다." + "<br><br>" + "인증 번호는 " + checkNum + " 입니다." + "<br><br>"
+				+ "해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+			helper.setFrom(setFrom); // 보내는사람 생략하면 정상작동을 안함
+			helper.setTo(toMail); // 받는사람 이메일
+			helper.setSubject(title); // 메일제목은 생략 가능
+			helper.setText(content); // 메일 내용
+
+			mailSender.send(message);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String num = Integer.toString(checkNum);
+
+		return num;
+
 	}
 
-	// 회원 가입 post
-	@PostMapping("/join.fr")
-	public String postJoin(UserVO vo) throws Exception {
-		log.info("post join");
 
-		userService.join(vo);
+	// 아이디 중복 확인
+	@ResponseBody
+	@PostMapping("/idCheck.fr")
+	public String idCheck(String id) throws Exception {
+		int result = userService.idCheck(id);
 
-		return null;
-
+		if (result != 0) {
+			return "fail"; // 중복아이디 존재
+		} else {
+			return "success";
+		}
 	}
+	
+	// 회원가입 페이지 이동
+	@RequestMapping("/join.fr")
+	public void join() throws Exception{
+		log.info("join");
+	}
+
+	// 회원 가입 처리 후, 메인페이지 이동
+	@RequestMapping("/joinProcess.fr")
+	public String joinProcess(UserVO vo) throws Exception {
+		log.info("join");
+
+		userService.joinProcess(vo);
+
+		return "redirect:/login/all.fr";
+	}
+
 }
